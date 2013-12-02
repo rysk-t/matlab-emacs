9d99a21d2a665b42e35d073cc60cbe4789137428
rysk_Macmini <rysk.takeuchi@gmail.com>
2013-12-02 19:33:38 +0900

    optimize matlab.el

--- matlab-highlight-cellbreak.el
+++ matlab-highlight-cellbreak.el
@@ -1,3 +1,11 @@
+;; matlab-highlight-cellbread.el --- simple script for matlab-mode MATLAB(R)
+;; 
+;; Highlighting Cellbreak Line (%% *)
+;; 
+;; Author: Ryosuke Takeuchi <rysk.takeuchi[at]gmail.com>
+;; Created: Mon Dec 2 2013
+;; Keywords: MATLAB(R), Emacs
+
 (defun matlab-highlight-cellbreak ()
   (interactive)
   (set-face-attribute
